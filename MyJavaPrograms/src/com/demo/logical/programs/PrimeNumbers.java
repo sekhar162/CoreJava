fc9e63ce8f85c81e2855c5d2deea9cac6eb0baec
sekharhuawei <reddi.shekhar@huawei.com>
2021-02-04 10:54:38 +0530

    Updated programs

--- MyJavaPrograms/src/com/demo/logical/programs/PrimeNumbers.java
+++ MyJavaPrograms/src/com/demo/logical/programs/PrimeNumbers.java
@@ -1,32 +1,30 @@
 package com.demo.logical.programs;
+
+import java.util.Scanner;
+
 //Prime numbers from 1 to 100 are :
 // 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
-class PrimeNumbers
-{
-   public static void main (String[] args)
-   {		
-       int i =0;
-       int num =0;
-       //Empty String
-       String  primeNumbers = "";
+class PrimeNumbers {
+	public static void main(String[] args) {
+		System.out.println("Please enter a number to print the prime numbers up to that number");
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		boolean isPrime = true;
+		for (int i = 2; i < n; i++) {
 
-       for (i = 1; i <= 100; i++)         
-       { 		  	  
-          int counter=0; 	  
-          for(num =i; num>=1; num--)
-	  {
-             if(i%num==0)
-	     {
- 		counter = counter + 1;
-	     }
-	  }
-	  if (counter ==2)
-	  {
-	     //Appended the Prime number to the String
-	     primeNumbers = primeNumbers + i + " ";
-	  }	
-       }	
-       System.out.println("Prime numbers from 1 to 100 are :");
-       System.out.println(primeNumbers);
-   }
+			for (int j = 2; j < i; j++) {
+
+				if (i % j == 0) {
+					isPrime = false;
+					break;
+				} else
+					isPrime = true;
+			}
+			if (isPrime) {
+				System.out.print(i + " ");
+			}
+
+		}
+
+	}
 }
